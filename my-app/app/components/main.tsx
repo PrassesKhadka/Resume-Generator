b7ff533347df736bbf3ce6fe4487106d8aec613a
Prasses Khadka <khadkaprasses@gmail.com>
2023-09-27 04:43:44 -0400

    Landing page designed

--- my-app/app/components/main.tsx
+++ my-app/app/components/main.tsx
@@ -6,21 +6,31 @@ import Link from "next/link";
 export const Main = () => {
   const [name, setName] = useState("");
   return (
-    <div>
-      <h1>Github Profile</h1>
-      <h2>Generate your Github Profile</h2>
-      <form>
-        <input
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-          type="text"
-          placeholder="Github Username"
-        ></input>
-        <Link href={`resume/${name}`}>
-          <button type='submit'>Generate</button>
-        </Link>
-      </form>
+    <div className="h-screen w-screen flex justify-center items-center">
+      <div className="flex flex-col items-center">
+        <h1 className="text-2xl font-bold">Github Profile</h1>
+        <h2 className="text-slate-400 -mt-2 mb-4">
+          Generate your Github Profile
+        </h2>
+        <form>
+          <input
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            type="text"
+            placeholder="Github Username"
+            className="p-1 outline-none border-2 border-slate-400 rounded-sm"
+          ></input>
+          <Link href={`resume/${name}`}>
+            <button
+              type="submit"
+              className="bg-orange hover:bg-red-200 text-white text-center text-sm pr-8 pl-8 pt-1 pb-1 ml-3 rounded-md"
+            >
+              Generate
+            </button>
+          </Link>
+        </form>
+      </div>
     </div>
   );
 };
--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -8,7 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-     
+      colors:{
+        'orange':'#de5a21',
+      }
     },
   },
   plugins: [],
